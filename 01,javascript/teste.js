e7309c9cb22c658398d774de8e6938b2e6ac1459
jeancoitino17-cmyk <jeancoitino17@gmail.com>
2026-04-16 22:14:13 -0300

    javascript

---
+++ 01,javascript/teste.js
@@ -0,0 +1,23 @@
+//  alert("Olá mundo!")
+// alert("SEGUNDA MENSAGEM")
+
+// let nome
+    
+// nome = "Jean"
+
+// alert("NOME :  " + nome)
+
+// let idade = 21
+// alert("IDADE : " + idade )
+
+// alert("NOME: " + nome + "\nIDADE" + idade)
+
+// nome = prompt("Digite seu nome:")
+// alert("Nome digitado : " + nome )
+
+
+let nota1
+nota1 = prompt("Digite a primeira nota: ")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert(nota1)
