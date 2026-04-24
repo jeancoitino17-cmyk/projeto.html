8aab8faeee215fedd33a9ee46ddab3417e8141d9
jeancoitino17-cmyk <jeancoitino17@gmail.com>
2026-04-24 18:42:02 -0300

    guilerme.portoes.js

---
+++ 01.javascrip/guilerme.Portoes.js
@@ -0,0 +1,13 @@
+// informacoes
+let clt = 16
+let estagiarios = 7
+let pj = 9
+let total
+// proseçamento
+total = clt + estagiarios + pj
+// entrada de dados
+clt = Number(prompt("Quantidade de CLTs:"));
+estagiarios = Number(prompt("Quantidade de estagiários:"));
+pj = Number(prompt("Quantidade de PJ:"));
+// saida
+console.log("Total de Portões: " + total)   
---
+++ 01.javascrip/pePequeno.js
@@ -0,0 +1,10 @@
+// infos
+let preco, quantidade
+let valorTotal
+// leiturara
+quantidade = Number(prompt("Quantidade:"))
+preco = Number(prompt("Preço: R$"))
+// proc
+valorTotal =  quantidade * preco
+// saidas
+console.log("Valor a receber R$" + valorTotal)
