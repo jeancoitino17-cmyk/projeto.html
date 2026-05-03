2fc8973f6b04fd636caf209f9ffbad4b9fb06ac7
unknown <jeancoitino17@gmail.com>
2026-05-03 18:13:42 -0300

    Finalizando projeto Sorteio com nomes de arquivos corrigidos

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 function generateNumber() {
-    const min = Math.ceil(document.querySelector(".input-min").value);
-    const max = Math.floor(document.querySelector(".input-max").value);
+    const min = Math.ceil(document.querySelector('.input-min').value);
+    const max = Math.floor(document.querySelector('.input-max').value);
 
-    if (min >= max) {
-        alert("O valor mínimo deve ser menor que o valor máximo!");
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // Lógica para verificar se é par ou ímpar
+    if (result % 2 === 0) {
+        alert(`O número ${result} é PAR`);
     } else {
-        const result = Math.floor(Math.random() * (max - min + 1)) + min;
-        alert("O número sorteado é: " + result);
+        alert(`O número ${result} é ÍMPAR`);
     }
 }
