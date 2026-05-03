7d11b0347fd744ce9760a61d420e836e644c3f94
unknown <jeancoitino17@gmail.com>
2026-05-03 18:10:18 -0300

    Finalizando projeto Sorteio com nomes de arquivos corrigidos

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function generateNumber() {
+    const min = Math.ceil(document.querySelector(".input-min").value);
+    const max = Math.floor(document.querySelector(".input-max").value);
+
+    if (min >= max) {
+        alert("O valor mínimo deve ser menor que o valor máximo!");
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert("O número sorteado é: " + result);
+    }
+}
